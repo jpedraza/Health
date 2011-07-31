84808add565044369a0aa50e6eb8c4552caac255
Nikita <nikita.kem@gmail.com>
2011-07-31 14:06:05 +0700

    Добавлена lazy-loading для сервисов и репозиториев.

--- Health/Health.Core/CoreKernel.cs
+++ Health/Health.Core/CoreKernel.cs
@@ -11,27 +11,41 @@ namespace Health.Core
 {
     public class CoreKernel : ICoreKernel
     {
+        private IRoleRepository<IRole> _roleRepo;
+        private IUserRepository<IUser> _userRepo;
+        private ICandidateRepository<ICandidate> _candRepo;
+        private IAuthorizationService<IUserCredential> _authServ;
+        private IRegistrationService<ICandidate> _regServ;
         protected IKernel DIKernel { get; set; }
 
         public CoreKernel(IKernel di_kernel)
         {
             DIKernel = di_kernel;
-            RoleRepo = DIKernel.Get<IRoleRepository<IRole>>(this);
-            UserRepo = DIKernel.Get<IUserRepository<IUser>>(this);
-            CandRepo = DIKernel.Get<ICandidateRepository<ICandidate>>(this);
-            RegServ = DIKernel.Get<IRegistrationService<ICandidate>>(this);
-            AuthServ = di_kernel.Get<IAuthorizationService<IUserCredential>>(this);
         }
 
-        public IRoleRepository<IRole> RoleRepo { get; private set; }
+        public IRoleRepository<IRole> RoleRepo
+        {
+            get { return _roleRepo ?? (_roleRepo = DIKernel.Get<IRoleRepository<IRole>>(this)); }
+        }
 
-        public IUserRepository<IUser> UserRepo { get; private set; }
+        public IUserRepository<IUser> UserRepo
+        {
+            get { return _userRepo ?? (_userRepo = DIKernel.Get<IUserRepository<IUser>>(this)); }
+        }
 
-        public ICandidateRepository<ICandidate> CandRepo { get; private set; }
+        public ICandidateRepository<ICandidate> CandRepo
+        {
+            get { return _candRepo ?? (_candRepo = DIKernel.Get<ICandidateRepository<ICandidate>>(this)); }
+        }
 
-        public IAuthorizationService<IUserCredential> AuthServ { get; private set; }
+        public IAuthorizationService<IUserCredential> AuthServ
+        {
+            get { return _authServ ?? (_authServ = DIKernel.Get<IAuthorizationService<IUserCredential>>(this)); }
+        }
 
-        public IRegistrationService<ICandidate> RegServ { get; private set; }
-
+        public IRegistrationService<ICandidate> RegServ
+        {
+            get { return _regServ ?? (_regServ = DIKernel.Get<IRegistrationService<ICandidate>>(this)); }
+        }
     }
 }
