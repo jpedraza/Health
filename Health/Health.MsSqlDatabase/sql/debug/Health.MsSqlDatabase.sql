334cf36868060b8727d2c6f3ed83d09719025e42
Nikita <nikita.kem@gmail.com>
2011-08-27 14:41:35 +0700

    Обновлены настройки проекта базы данных.

--- Health/Health.MsSqlDatabase/sql/debug/Health.MsSqlDatabase.sql
+++ Health/Health.MsSqlDatabase/sql/debug/Health.MsSqlDatabase.sql
@@ -26,31 +26,132 @@ BEGIN
 END
 
 GO
-
-IF NOT EXISTS (SELECT 1 FROM [master].[dbo].[sysdatabases] WHERE [name] = N'$(DatabaseName)')
+IF (DB_ID(N'$(DatabaseName)') IS NOT NULL) 
 BEGIN
-    RAISERROR(N'Не удается развернуть этот скрипт обновления на конечном объекте CRASH-PC. База данных Health.MsSqlDatabase, для которой создан этот скрипт, не существует на данном сервере.', 16, 127) WITH NOWAIT
-    RETURN
+    ALTER DATABASE [$(DatabaseName)]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE [$(DatabaseName)];
 END
 
 GO
+PRINT N'Выполняется создание $(DatabaseName)...'
+GO
+CREATE DATABASE [$(DatabaseName)]
+    ON 
+    PRIMARY(NAME = [Health_MsSqlDatabase], FILENAME = N'$(DefaultDataPath)Health_MsSqlDatabase.mdf')
+    LOG ON (NAME = [Health_MsSqlDatabase_log], FILENAME = N'$(DefaultLogPath)Health_MsSqlDatabase_log.ldf') COLLATE SQL_Latin1_General_CP1_CI_AS
+GO
+EXECUTE sp_dbcmptlevel [$(DatabaseName)], 100;
 
-IF (@@servername != 'CRASH-PC')
-BEGIN
-    RAISERROR(N'Имя сервера в сценарии построения %s не соответствует имени конечного сервера %s. Проверьте, правильны ли параметры проекта базы данных и не устарел ли сценарий построения.', 16, 127,N'CRASH-PC',@@servername) WITH NOWAIT
-    RETURN
-END
 
 GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ANSI_NULLS ON,
+                ANSI_PADDING ON,
+                ANSI_WARNINGS ON,
+                ARITHABORT ON,
+                CONCAT_NULL_YIELDS_NULL ON,
+                NUMERIC_ROUNDABORT OFF,
+                QUOTED_IDENTIFIER ON,
+                ANSI_NULL_DEFAULT ON,
+                CURSOR_DEFAULT LOCAL,
+                RECOVERY FULL,
+                CURSOR_CLOSE_ON_COMMIT OFF,
+                AUTO_CREATE_STATISTICS ON,
+                AUTO_SHRINK OFF,
+                AUTO_UPDATE_STATISTICS ON,
+                RECURSIVE_TRIGGERS OFF 
+            WITH ROLLBACK IMMEDIATE;
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_CLOSE OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
 
-IF CAST(DATABASEPROPERTY(N'$(DatabaseName)','IsReadOnly') as bit) = 1
-BEGIN
-    RAISERROR(N'Не удается развернуть этот скрипт обновления, так как для базы данных %s, для которой он создан, задано состояние READ_ONLY.', 16, 127, N'$(DatabaseName)') WITH NOWAIT
-    RETURN
-END
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ALLOW_SNAPSHOT_ISOLATION OFF;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET READ_COMMITTED_SNAPSHOT OFF;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_UPDATE_STATISTICS_ASYNC OFF,
+                PAGE_VERIFY NONE,
+                DATE_CORRELATION_OPTIMIZATION OFF,
+                DISABLE_BROKER,
+                PARAMETERIZATION SIMPLE,
+                SUPPLEMENTAL_LOGGING OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET TRUSTWORTHY OFF,
+        DB_CHAINING OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'Параметры базы данных изменить нельзя. Применить эти параметры может только пользователь SysAdmin.';
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET HONOR_BROKER_PRIORITY OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'Параметры базы данных изменить нельзя. Применить эти параметры может только пользователь SysAdmin.';
+    END
+
 
 GO
 USE [$(DatabaseName)]
+GO
+IF fulltextserviceproperty(N'IsFulltextInstalled') = 1
+    EXECUTE sp_fulltext_database 'enable';
+
+
 GO
 /*
  Шаблон скрипта после развертывания							
@@ -71,6 +172,33 @@ PRINT N'Выполняется удаление Разрешения...';
 GO
 REVOKE CONNECT TO [dbo] CASCADE
     AS [dbo];
+
+
+GO
+PRINT N'Выполняется удаление [AutoCreatedLocal]...';
+
+
+GO
+DROP ROUTE [AutoCreatedLocal];
+
+
+GO
+PRINT N'Выполняется создание [dbo].[User]...';
+
+
+GO
+CREATE TABLE [dbo].[User] (
+    [UserId]    INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (MAX) NOT NULL,
+    [LastName]  NVARCHAR (MAX) NOT NULL,
+    [ThirdName] NVARCHAR (MAX) NOT NULL,
+    [Login]     NVARCHAR (MAX) NOT NULL,
+    [Password]  NVARCHAR (MAX) NOT NULL,
+    [RoleId]    INT            NOT NULL,
+    [Birthday]  DATETIME       NOT NULL,
+    [Token]     NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC) WITH (ALLOW_PAGE_LOCKS = ON, ALLOW_ROW_LOCKS = ON, PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
+);
 
 
 GO
@@ -112,6 +240,13 @@ BEGIN
 	select @is_even_week, @week_day
 END
 GO
+-- Выполняется этап рефакторинга для обновления развернутых журналов транзакций на целевом сервере
+CREATE TABLE  [dbo].[__RefactorLog] (OperationKey UNIQUEIDENTIFIER NOT NULL PRIMARY KEY)
+GO
+sp_addextendedproperty N'microsoft_database_tools_support', N'refactoring log', N'schema', N'dbo', N'table', N'__RefactorLog'
+GO
+
+GO
 /*
 Шаблон скрипта после развертывания							
 --------------------------------------------------------------------------------------
@@ -123,5 +258,25 @@ GO
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        DECLARE @VarDecimalSupported AS BIT;
+        SELECT @VarDecimalSupported = 0;
+        IF ((ServerProperty(N'EngineEdition') = 3)
+            AND (((@@microsoftversion / power(2, 24) = 9)
+                  AND (@@microsoftversion & 0xffff >= 3024))
+                 OR ((@@microsoftversion / power(2, 24) = 10)
+                     AND (@@microsoftversion & 0xffff >= 1600))))
+            SELECT @VarDecimalSupported = 1;
+        IF (@VarDecimalSupported > 0)
+            BEGIN
+                EXECUTE sp_db_vardecimal_storage_format N'$(DatabaseName)', 'ON';
+            END
+    END
+
 
 GO
