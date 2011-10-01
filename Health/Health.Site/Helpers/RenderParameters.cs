b49338aa90693618723eb8588710ab9cedc3abf4
Nikita <nikita.kem@gmail.com>
2011-10-01 20:52:45 +0700

    Исправлены конфликты.

---
+++ Health/Health.Site/Helpers/RenderParameters.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web;
+using Health.Core.Entities.POCO;
+using Health.Site.Factory;
+namespace Health.Site.Helpers
+{
+    public static class RenderParameters
+    {
+        public static HtmlString GetLayoutForParameter(this HtmlHelper helper, Parameter parameter, string name_prefix)
+        {
+            var factory = new RenderingFactory(parameter, name_prefix);
+            return factory.GetLayout();
+        }
+    }
+}
