3e7ddd7b318dcc1f8a77549fe0d187661a5ed9d4
Denis <denis.kem@gmail.com>
2011-11-11 19:52:18 +0700

    Добавлена сущность Медицинские операции и лечения конкретного пациента.  Также к этой сущности прописаны мета-данные.

---
+++ Health/Health.Site/Models/Metadata/PatientSurgeryMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Health.Core.Entities.POCO;
+using Health.Core.TypeProvider;
+using System.Collections;
+using System.Collections.Generic;
+namespace Health.Site.Models.Metadata
+{
+    public class PatientSurgeryMetadata : SurgeryMetadata
+    {
+        [DisplayName("Дата выполнения операции/лечения")]
+        public virtual DateTime DateSurgery { get; set; }
+
+        [DisplayName("Статус выполнени операции/лечения")]
+        public virtual bool Status { get; set; }
+    }
+
+    public class PatientSurgeryFormMetadata : PatientSurgeryMetadata
+    {
+        [Required(ErrorMessage = "Введите дату выполнения операции/лечения")]
+        public override DateTime DateSurgery { get; set; }
+
+        [Required(ErrorMessage = "Введите описание операции/лечения")]
+        public override string Description { get; set; }
+
+        [Required(ErrorMessage = "Введите название операции/лечения")]
+        public override string Name { get; set; }
+    }
+}
