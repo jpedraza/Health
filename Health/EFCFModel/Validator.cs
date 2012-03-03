af761a80cda43faa2d7a740e7f11b4762acab7dc
Nikita <nikita.kem@gmail.com>
2012-03-03 18:57:55 +0700

    Добавлен простой валидатор для объектов.

---
+++ Health/EFCFModel/Validator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+
+namespace EFCFModel
+{
+    public class Validator
+    {
+        public static IEnumerable<ValidationResult> Validate(object component)
+        {
+            return from descriptor in TypeDescriptor.GetProperties(component).Cast<PropertyDescriptor>()
+                   from validation in descriptor.Attributes.OfType<ValidationAttribute>()
+                   where !validation.IsValid(descriptor.GetValue(component))
+                   select new ValidationResult(
+                       validation.ErrorMessage ?? string.Format(CultureInfo.CurrentUICulture, "{0} validation failed.", validation.GetType().Name),
+                       new[] { descriptor.Name });
+        }
+    }
+}
